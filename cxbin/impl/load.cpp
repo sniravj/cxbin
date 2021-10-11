b1894f258d7344840764a94fc27505c262707dce
anoob <anoob@sina.cn>
2021-10-11 15:59:26 +0800

    1

--- cxbin/impl/load.cpp
+++ cxbin/impl/load.cpp
@@ -18,6 +18,8 @@ namespace cxbin
 		if (!f)
 		{
 			formartPrint(tracer, "load T : load file error for [%s]", strerror(errno));
+			if (tracer)
+				tracer->failed("Open File Error.");
 			return models;
 		}
 
