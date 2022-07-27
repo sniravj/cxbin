d22ad2476164feb7fa49bcef4f1ccef428afdf64
yanyixun <yanyixun@creality.com>
2022-07-27 12:17:06 +0800

    feat[cxbin]：cxbin 格式的读写按版本分开，以兼容之前的 cxbin 文件
    Change-Id: I14ae1e6fc25568bd6a6c075c72b3fdec5ab4347c

--- cxbin/cx/reader.cpp
+++ cxbin/cx/reader.cpp
@@ -1,5 +1,6 @@
 #include "reader.h"
 #include <zlib.h>
+#include <map>
 #include "trimesh2/TriMesh.h"
 
 #include "cxbin/convert.h"
@@ -9,6 +10,70 @@
 namespace cxbin
 {
 	bool loadCXBin0(FILE* f, size_t fileSize, trimesh::TriMesh& out, ccglobal::Tracer* tracer)
+	{
+		bool success = false;
+		int vertNum = 0;
+		int faceNum = 0;
+		int totalNum = 0;
+		int compressNum = 0;
+
+		bool need_swap = trimesh::we_are_big_endian();
+		fread(&totalNum, sizeof(int), 1, f);
+		fread(&vertNum, sizeof(int), 1, f);
+		fread(&faceNum, sizeof(int), 1, f);
+		fread(&compressNum, sizeof(int), 1, f);
+		if (need_swap)
+		{
+			trimesh::swap_int(totalNum);
+			trimesh::swap_int(vertNum);
+			trimesh::swap_int(faceNum);
+			trimesh::swap_int(compressNum);
+		}
+		unsigned char* data = new unsigned char[totalNum];
+		unsigned char* cdata = new unsigned char[compressNum];
+		formartPrint(tracer, "loadCXBin0 load %d %d %d %d.", totalNum, (int)compressNum, vertNum, faceNum);
+
+		uLong uTotalNum = (uLong)totalNum;
+		if (fread(cdata, 1, compressNum, f))
+		{
+			if (uncompress(data, &uTotalNum, cdata, compressNum) == Z_OK)
+			{
+				if (vertNum > 0)
+				{
+					out.vertices.resize(vertNum);
+					memcpy(&out.vertices.at(0), data, vertNum * sizeof(trimesh::vec3));
+				}
+				if (faceNum > 0)
+				{
+					out.faces.resize(faceNum);
+					memcpy(&out.faces.at(0), data + vertNum * sizeof(trimesh::vec3), faceNum * sizeof(trimesh::ivec3));
+				}
+
+				success = true;
+			}
+		}
+
+		delete[]data;
+		delete[]cdata;
+
+		if (true == success)
+		{
+			if (tracer)
+			{
+				tracer->success();
+			}
+		}
+		else
+		{
+			if (tracer)
+			{
+				tracer->failed("file read failed");
+			}
+		}
+		return success;
+	}
+
+	bool loadCXBin1(FILE* f, size_t fileSize, trimesh::TriMesh& out, ccglobal::Tracer* tracer)
 	{
 		bool success = false;
 		int vertNum = 0;
@@ -161,6 +226,11 @@ namespace cxbin
 
 	bool loadCXBin(FILE* f, size_t fileSize, trimesh::TriMesh& out, ccglobal::Tracer* tracer)
 	{
+		static std::map<int, std::function<bool(FILE*, size_t, trimesh::TriMesh&, ccglobal::Tracer*)>> verLFuncMap = {
+		{0, loadCXBin0},
+		{1, loadCXBin1}
+		};
+
 		int version = -1;
 		fread(&version, sizeof(int), 1, f);
 
@@ -170,8 +240,8 @@ namespace cxbin
 
 		char data[12] = "";
 		fread(data, 1, 12, f);
-		if (version == 0)
-			return loadCXBin0(f, fileSize, out, tracer);
+		if (version < verLFuncMap.size())
+			return verLFuncMap[version](f, fileSize, out, tracer);
 		return false;
 	}
 }
--- cxbin/cx/writer.cpp
+++ cxbin/cx/writer.cpp
@@ -15,6 +15,39 @@ namespace cxbin
 		bool success = false;
 		int vertNum = mesh->vertices.size();
 		int faceNum = mesh->faces.size();
+
+		int totalNum = vertNum * sizeof(trimesh::vec3) + faceNum * sizeof(trimesh::ivec3);
+		fwrite((const char*)&totalNum, sizeof(int), 1, out);
+		fwrite((const char*)&vertNum, sizeof(int), 1, out);
+		fwrite((const char*)&faceNum, sizeof(int), 1, out);
+		uLong compressNum = compressBound(totalNum);
+		unsigned char* data = new unsigned char[totalNum];
+		unsigned char* cdata = new unsigned char[compressNum];
+
+		if (vertNum > 0)
+			memcpy(data, &mesh->vertices.at(0), vertNum * sizeof(trimesh::vec3));
+		if (faceNum > 0)
+			memcpy(data + vertNum * sizeof(trimesh::vec3), &mesh->faces.at(0), faceNum * sizeof(trimesh::ivec3));
+		if (compress(cdata, &compressNum, data, totalNum) == Z_OK)
+		{
+			formartPrint(tracer, "writeCXBin0 write %d %d %d %d.", totalNum, (int)compressNum, vertNum, faceNum);
+
+			int iCompressNum = (int)compressNum;
+			fwrite((const char*)&iCompressNum, sizeof(int), 1, out);
+			fwrite((const char*)cdata, 1, compressNum, out);
+			success = true;
+		}
+
+		delete[]data;
+		delete[]cdata;
+		return success;
+	}
+
+	bool writeCXBin1(FILE* out, trimesh::TriMesh* mesh, ccglobal::Tracer* tracer)
+	{
+		bool success = false;
+		int vertNum = mesh->vertices.size();
+		int faceNum = mesh->faces.size();
 		int UVNum = mesh->UVs.size();
 		int faceUVNum = mesh->faceUVs.size();
 		int textureIDNum = mesh->textureIDs.size();
@@ -134,11 +167,16 @@ namespace cxbin
 
 	bool writeCXBin(FILE* out, trimesh::TriMesh* mesh, ccglobal::Tracer* tracer)
 	{
+		static std::map<int, std::function<bool(FILE*, trimesh::TriMesh*, ccglobal::Tracer*)>> verWFuncMap = {
+			{0, writeCXBin0},
+			{1, writeCXBin1}
+		};
+
 		fwrite((const char*)&version, sizeof(int), 1, out);
 		char data[12] = "\niwlskdfjad";
 		fwrite(data, 1, 12, out);
-		if(version == 0)
-			return writeCXBin0(out, mesh, tracer);
+		if(version < verWFuncMap.size())
+			return verWFuncMap[version](out, mesh, tracer);
 		return false;
 	}
 }
